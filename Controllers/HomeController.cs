ae9659d3c62da17858e9764de48628f5e89c1460
Rafał Kwiecień <53783787+Arcanagos@users.noreply.github.com>
2022-01-13 21:42:35 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Zadanie_lab3.Models;
+
+namespace Zadanie_lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Create(FormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(model.Ilosc_powtorzen >= 1 && model.Ilosc_powtorzen <= 5)
+                {
+                    return View("Created", model);
+                }
+                else
+                {
+                    return View("Index");
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/FormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zadanie_lab3.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Imie")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Liczba powtórzeń")]
+        [Required]
+        public int Ilosc_powtorzen { get; set; }
+    }
+}
